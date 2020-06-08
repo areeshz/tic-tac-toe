6ee0b4527a5ed0532bd0160171896ead71bd5744
areeshz <azindani786@gmail.com>
2020-06-08 17:49:19 -0400

    Clean up logic for checking for the winner

--- assets/scripts/game/game-logic.js
+++ assets/scripts/game/game-logic.js
@@ -4,63 +4,45 @@ const store = require('./../store.js')
 const checkForWinner = (index) => {
   // IDEA - make a nested array of indices of the possible winning combinations, iterate through each nested array, use those indices to check the cells in the game board for a winner. If a winner is identified, record that info, disable the board, do other necessary things, and break the loop. Could use an "any" or "some" iterator possibly
   const combos = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
   ]
 
-  // console.log(`index is ${index}`)
+  // Update local store with latest game move
   store.game.cells[index] = store.game.player1Move ? 'x' : 'o'
-  // console.log(store.game.cells)
+  const cells = store.game.cells
+  let winnerFound
+  let winner
 
-  // Conditional chain to check for winner on current board state
-  if (store.game.cells[0] && store.game.cells[1] && store.game.cells[2] && store.game.cells[0] === store.game.cells[1] && store.game.cells[1] === store.game.cells[2]) {
-    // Check if first row contains winner
-    console.log(`${store.game.cells[0]} wins, row 1!`)
-    console.log(store.game.cells)
-    $('.game-box').off('click') // disables board
-  } else if (store.game.cells[3] && store.game.cells[4] && store.game.cells[5] && store.game.cells[3] === store.game.cells[4] && store.game.cells[4] === store.game.cells[5]) {
-    // Check if second row contains winner
-    console.log(`${store.game.cells[3]} wins, row 2!`)
-    console.log(store.game.cells)
-    $('.game-box').off('click') // disables board
-  } else if (store.game.cells[6] && store.game.cells[7] && store.game.cells[8] && store.game.cells[6] === store.game.cells[7] && store.game.cells[7] === store.game.cells[8]) {
-    // Check if third row contains winner
-    console.log(`${store.game.cells[6]} wins, row 3!`)
-    console.log(store.game.cells)
-    $('.game-box').off('click') // disables board
-  } else if (store.game.cells[0] && store.game.cells[3] && store.game.cells[6] && store.game.cells[0] === store.game.cells[3] && store.game.cells[3] === store.game.cells[6]) {
-    // Check if first column contains winner
-    console.log(`${store.game.cells[0]} wins, col 1!`)
-    console.log(store.game.cells)
-    $('.game-box').off('click') // disables board
-  } else if (store.game.cells[1] && store.game.cells[4] && store.game.cells[7] && store.game.cells[1] === store.game.cells[4] && store.game.cells[4] === store.game.cells[7]) {
-    // Check if second column contains winner
-    console.log(`${store.game.cells[1]} wins, col 2!`)
-    console.log(store.game.cells)
-    $('.game-box').off('click') // disables board
-  } else if (store.game.cells[2] && store.game.cells[5] && store.game.cells[8] && store.game.cells[2] === store.game.cells[5] && store.game.cells[5] === store.game.cells[8]) {
-    // Check if third column contains winner
-    console.log(`${store.game.cells[2]} wins, col 3!`)
-    console.log(store.game.cells)
-    $('.game-box').off('click') // disables board
-  } else if (store.game.cells[0] && store.game.cells[4] && store.game.cells[8] && store.game.cells[0] === store.game.cells[4] && store.game.cells[4] === store.game.cells[8]) {
-    // Check if first diagonal contains winner
-    console.log(store.game.cells)
-    console.log(`${store.game.cells[0]} wins, diag 1!`)
-    $('.game-box').off('click') // disables board
-  } else if (store.game.cells[2] && store.game.cells[4] && store.game.cells[6] && store.game.cells[2] === store.game.cells[4] && store.game.cells[4] === store.game.cells[6]) {
-    // Check if second diagonal contains winner
-    console.log(`${store.game.cells[2]} wins, diag 2!`)
-    console.log(store.game.cells)
-    $('.game-box').off('click') // disables board
-  } else if (store.game.cells.every(cell => cell !== '')) {
-    console.log("it's a tie!")
+  // Check for any winning combos
+  for (let i = 0; i < combos.length; i++) {
+    const combo = combos[i]
+    if (combo.every(index => cells[index] !== '')) {
+      if (cells[combo[0]] === cells[combo[1]] && cells[combo[1]] === cells[combo[2]]) {
+        console.log(`found a winner`)
+        winnerFound = true
+        console.log(`cells[combo[0]] is ${cells[combo[0]]}`)
+        winner = cells[combo[0]]
+        break
+      }
+    }
+  }
+
+  if (!winnerFound && cells.every(cell => cell !== '')) {
+    winnerFound = true
+    winner = 'tie'
+  }
+
+  if (winnerFound) {
+    switch (winner) {
+      case 'x':
+        console.log(`X is the winner!`)
+        break
+      case 'o':
+        console.log(`O is the winner!`)
+        break
+      case 'tie':
+        console.log(`oof, it's a tie!`)
+    }
   }
 
   store.game.player1Move = !store.game.player1Move // Switch turn to next user
